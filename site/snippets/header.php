b00c22a7cac4e5bd8aaf64b74cee421c49721e33
Martin Nicholson <recyclebing+github@gmail.com>
2018-09-03 00:17:08 +0100

    Formspring form integrated & partially styled

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,7 +12,7 @@
         <?= css('https://fonts.googleapis.com/css?family=Bitter|Cabin+Sketch|Dosis') ?>
         <?= css('assets/css/style.css') ?>
     </head>
-    <body>
+    <body id="<?= $page->title() ?>">
         <!--[if lt IE 8]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,21 +2,35 @@
 
     <main class="main" role="main">
         
-        <div class="wrap">
-            <h1><?= $page->title()->html() ?></h1>      
-            <div class="intro text">
-                <?= $page->intro()->kirbytext() ?>
-            </div>    
-            <hr />      
-        </div>
-        
         <div class="wrap wide">
-            <h2>Get in Touch</h2>
+
+            <form action="https://formspree.io/recyclebing@gmail.com" method="POST">
+                <h2>Contact Us</h2>
+                <p>Thanks for your interest. Please send your message via the following form. We will respond ASAP.</p>
+                <p>
+                    <label for="name">Your name</label>
+                    <input type="text" name="name">
+                </p>
+                <p>
+                    <label for="_replyto">Your email</label>
+                    <input type="email" name="_replyto">
+                </p>
+                <select name="subject">
+                    <option>Subject of your enquiry:</option>
+                    <option>---</option>
+                    <option value="1">Graphic Design</option>
+                    <option value="2">Web Design</option>
+                    <option value="3">Product Design</option>
+                    <option value="4">Music Videos</option>
+                    <option value="5">Billing</option>
+                    <option value="6">Job Opportunities</option>
+                </select>
+                <br>
+                <textarea placeholder="Type your message here"></textarea>
+                <br>
+                <input type="submit" value="Send">
+            </form>
             
-        </div>
-            
-        <div class="contact-twitter text wrap cf">
-            <?= $page->text()->kirbytext() ?>
         </div>
         
     </main>
